3a02e966139c947414240872934827db6aed81bb
Tarpelite <tarpelite_chan@foxmail.com>
2019-11-18 13:32:02 +0800

    fix bugs

--- src/biunilm/run_KG.py
+++ src/biunilm/run_KG.py
@@ -335,7 +335,7 @@ def main():
             model_recover = torch.load(
                 args.model_recover_path, map_location='cpu')
             global_step = 0
-        model = BertForPreTrainingLossMask.from_pretrained(
+        model = BertForUniKG.from_pretrained(
             args.bert_model, state_dict=model_recover, num_labels=cls_num_labels, num_rel=0, type_vocab_size=type_vocab_size, config_path=args.config_path, task_idx=3, num_sentlvl_labels=num_sentlvl_labels, max_position_embeddings=args.max_position_embeddings, label_smoothing=args.label_smoothing, fp32_embedding=args.fp32_embedding, relax_projection=relax_projection, new_pos_ids=args.new_pos_ids, ffn_type=args.ffn_type, hidden_dropout_prob=args.hidden_dropout_prob, attention_probs_dropout_prob=args.attention_probs_dropout_prob, num_qkv=args.num_qkv, seg_emb=args.seg_emb)
     if args.local_rank == 0:
         dist.barrier()
